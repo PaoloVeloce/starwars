aebf1c56cad8f8cc88650a5ff50a58f756694320
Paolo Veloce <pablo.veloce@gmail.com>
2016-06-27 22:39:57 +0300

    Install ejs, enrubied javascript for loop for showing array of films

--- index.js
+++ index.js
@@ -3,17 +3,28 @@ var express = require('express');
 // specify app as new express application
 var app = express();
 
+app.set('view engine', 'ejs');
+
 // Routes
 
 // Home server get request from client
 app.get('/', function(req, res) {
-	res.send("This is server response on home page!");
+	res.render('home', {
+		// that could be seen in home.js if we enruby <%= title %> here
+		title: "Star Wars Movies",
+		movies: ["The First Movie", "The Second Movie", "The Third Movie"]
+	});
 });
 
 // movie single
 app.get('/stawarepi/:episode_number?', function(req, res) {
 	var episode_number = req.params.episode_number;
 	res.send("This is the page for episode " + episode_number);
+});
+
+// notFound
+app.get('*', function(req, res) {
+	res.send("This is not the page you are looking for");
 })
 
 // listen on specific port
